b0d87f6ae5e365f2a513598e6d64c7a665bac788
shereen <shereen@DESKTOP-AHOU8JD>
2023-09-25 01:41:33 +0300

    Add project files.

---
+++ Model/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace mvc_Lap1.Model
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string department { get; set; }
+        public float salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"id= {id} Name ={name} Address = {address} Department = {department} Salary = {salary}";
+
+        }
+    }
+}
---
+++ controller/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc_Lap1.Model;
+
+namespace mvc_Lap1.controller
+{
+    public class EmployeeController : Controller
+    {
+       
+        public IActionResult GetEmployeeData()
+        {
+            List<Department> department = new List<Department>();
+            department.Add(new Department() { id=1,name="it"});
+            department.Add(new Department() { id=2,name="is"});
+            department.Add(new Department() { id=1,name="it"});
+            department.Add(new Department() { id=4,name="DS"});
+           
+            Employee EmployeeObj = new Employee()
+            {
+                id = 10,
+                name = "shereen",
+                address = "cairo",
+                //department =department.FirstOrDefault(e=>e.id==4).name,
+                department ="It",
+                salary = 5000
+             
+                };
+
+
+            //return View("employee", EmployeeObj);
+            return View("GetEmployeeData", EmployeeObj);
+            
+        }
+    }
+
+    internal class Department
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
